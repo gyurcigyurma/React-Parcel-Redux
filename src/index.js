df84fb747863b761e253e7cb30ef70fac38081d9
Gyurci <gyurxi@gmail.com>
2024-02-19 14:27:26 +0100

    with redux and redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { App } from "./components/App";
+import { Provider } from "react-redux";
+import App from "./components/App";
+import store from "./store/store";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
