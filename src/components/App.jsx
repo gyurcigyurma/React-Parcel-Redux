6452360b82f3691254fabe761e06f0ba8284c885
Gyurci <gyurxi@gmail.com>
2024-02-19 15:18:46 +0100

    tailwind added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from './Counter';
+import '../index.css';
 
 function App() {
   const count = useSelector((state) => state.counter.value);
@@ -7,13 +8,9 @@ function App() {
 
   return (
     <div>
-      <button aria-label="Increment value" onClick={() => dispatch(increment())}>
-        Increment
-      </button>
-      <span>{count}</span>
-      <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
-        Decrement
-      </button>
+      <button aria-label="Increment value" onClick={() => dispatch(increment())} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 m-1" >Increment</button>
+      <span className='w-11 inline-block border-2 border-gray-600'>{count}</span>
+      <button aria-label="Increment value" onClick={() => dispatch(decrement())} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 m-1" >Decrement</button>
     </div>
   );
 }
